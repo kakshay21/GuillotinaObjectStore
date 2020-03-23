0c487aff7eaccdbf3a0311347c4e4fd0fac0531d
Kumar Akshay <k.akshay9721@gmail.com>
2020-03-24 03:24:35 +0530

    use rocksdb 0.13 instead of custom one

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use std::{io, thread};
 use futures::sync::oneshot;
 use futures::Future;
 use grpcio::{ChannelBuilder, Environment, ResourceQuota, ServerBuilder};
-use rocksdb::{DB, Writable};
+use rocksdb::{DB};
 use transaction_grpc::Transaction;
 
 #[derive(Clone)]
@@ -37,13 +37,17 @@ fn db_delete(db: &DB, key: &str) -> bool {
     return val.is_ok();
 }
 
+fn db_get_value(db: &DB, key: &str) -> String {
+    let value = db.get(key.as_bytes()).unwrap().unwrap();
+    return String::from_utf8(value).unwrap();
+}
+
 impl Transaction for TransactionService {
     fn start_transaction(&mut self, ctx: ::grpcio::RpcContext, req: transaction::StartTransactionRequest, sink: ::grpcio::UnarySink<transaction::TxnId>) {
         let write = req.get_user();
         let db: DB = intialize_db("localdb");
         db_write(&db, write, "1234");
-        let r = db.get(write.as_bytes()).unwrap().unwrap();
-        let value = r.to_utf8().unwrap();
+        let value = db_get_value(&db, write);
         let tid: u64 = value.parse().unwrap();
         let mut txn = transaction::TxnId::default();
         txn.set_tid(tid);
